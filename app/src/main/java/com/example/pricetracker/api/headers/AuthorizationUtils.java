d56183745df1a37ee92bd7aab9faa8e4f641b91f
Adam <adamj1215@gmail.com>
2023-10-22 00:29:25 +0200

    rename

--- app/src/main/java/com/example/pricetracker/api/headers/AuthorizationUtils.java
+++ app/src/main/java/com/example/pricetracker/api/headers/AuthorizationUtils.java
@@ -4,19 +4,19 @@ import com.example.pricetracker.dto.response.AuthTokenResponse;
 
 public class AuthorizationUtils {
 
-    private static AuthTokenResponse authentication = null;
+    private static AuthTokenResponse authorization = null;
 
     public static String getAuthTokenFormatted() throws AuthTokenException {
-        if(authentication == null || authentication.getAccessToken() == null || authentication.getAccessToken().isEmpty()) {
+        if(authorization == null || authorization.getAccessToken() == null || authorization.getAccessToken().isEmpty()) {
             throw new AuthTokenException("Authorization token is not set!");
         }
-        return String.format("Bearer %s", authentication.getAccessToken());
+        return String.format("Bearer %s", authorization.getAccessToken());
     }
 
     public static void setAuthorizationData(AuthTokenResponse authTokenResponse) throws AuthTokenException {
         if(authTokenResponse.getAccessToken() == null || authTokenResponse.getAccessToken().isEmpty()) {
             throw new AuthTokenException("Authorization response does not contain proper token!");
         }
-        authentication = authTokenResponse;
+        authorization = authTokenResponse;
     }
 }
