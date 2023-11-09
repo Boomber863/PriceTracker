d215a54ab8f9ca2a229ff8e6e2fdaa18d421588e
Adam <adamj1215@gmail.com>
2023-11-09 20:36:50 +0100

    added items combobox and string constants in xml

--- app/src/main/java/com/example/pricetracker/HomepageActivity.java
+++ app/src/main/java/com/example/pricetracker/HomepageActivity.java
@@ -2,18 +2,15 @@ package com.example.pricetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.AutoCompleteTextView;
 
-import com.example.pricetracker.api.headers.AuthTokenException;
-import com.example.pricetracker.api.headers.AuthorizationUtils;
 import com.example.pricetracker.api.provider.ItemServiceProvider;
 import com.example.pricetracker.dto.response.ItemResponse;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -22,23 +19,35 @@ import retrofit2.Response;
 
 public class HomepageActivity extends AppCompatActivity {
 
+    private AutoCompleteTextView autoCompleteTextView;
+    private ArrayAdapter<ItemResponse> itemsAdapter;
+    private List<ItemResponse> items;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, R.layout.list_item, items);
+        autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+        autoCompleteTextView.setAdapter(itemsAdapter);
+
         getAvailableItems();
     }
 
     private void getAvailableItems() {
-        Call<List<ItemResponse>> items = ItemServiceProvider.getInstance().getItems();
-        items.enqueue(new Callback<List<ItemResponse>>() {
+        Call<List<ItemResponse>> itemsCall = ItemServiceProvider.getInstance().getItems();
+        itemsCall.enqueue(new Callback<List<ItemResponse>>() {
             @Override
             public void onResponse(Call<List<ItemResponse>> call, Response<List<ItemResponse>> response) {
                 if(!response.isSuccessful() || response.body() == null) {
                     Log.e("ERROR", "Bad response for getItems");
                     return;
                 }
-                String test = response.body().toString();
-                Log.i("GOT ITEMS", test);
+                items.addAll(response.body());
+                itemsAdapter.notifyDataSetChanged();
             }
 
             @Override
