033667975c8e7a3abccf57641036b1d235fd7dad
Mateusz Duda <mateusz.duda2@gmail.com>
2023-11-15 21:28:46 +0100

    changed colors

--- app/src/main/java/com/example/pricetracker/SignupActivity.java
+++ app/src/main/java/com/example/pricetracker/SignupActivity.java
@@ -1,78 +1,3 @@
-<<<<<<< HEAD
-package com.example.pricetracker;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.pricetracker.api.headers.AuthTokenException;
-import com.example.pricetracker.api.headers.AuthorizationUtils;
-import com.example.pricetracker.api.provider.AuthorizationServiceProvider;
-import com.example.pricetracker.components.CustomToast;
-import com.example.pricetracker.dto.request.RegistrationRequest;
-import com.example.pricetracker.dto.response.AuthTokenResponse;
-import com.example.pricetracker.dto.response.SignupResponse;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-public class SignupActivity extends AppCompatActivity {
-
-    private EditText usernameEditText, emailEditText, passwordEditText;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
-
-        usernameEditText = findViewById(R.id.signupUsername);
-        emailEditText = findViewById(R.id.signupEmail);
-        passwordEditText = findViewById(R.id.signupPassword);
-        Button signupButton = findViewById(R.id.signupButton);
-
-        signupButton.setOnClickListener(e -> onSignUpClicked());
-    }
-
-    private void onSignUpClicked() {
-
-        String username = usernameEditText.getText().toString();
-        String email = emailEditText.getText().toString();
-        String password = passwordEditText.getText().toString();
-        Call<SignupResponse> call = AuthorizationServiceProvider
-                .getInstance()
-                .registerUser(new RegistrationRequest(username, email, password));
-        call.enqueue(new Callback<SignupResponse>() {
-            @Override
-            public void onResponse(Call<SignupResponse> call, Response<SignupResponse> response) {
-                if (!response.isSuccessful() || response.body() == null) {
-                    CustomToast.showToastShort(SignupActivity.this, "Email already used");
-                    Log.e("ERROR", "Bad request for signup");
-                    return;
-                }
-                final SignupResponse signupResponse = response.body();
-                try {
-                    AuthorizationUtils.setAuthorizationData(signupResponse.getToken());
-                    Log.i("SIGNUP SUCCESS", "User successfully signed up");
-                    startActivity(new Intent(SignupActivity.this, HomepageActivity.class));
-                } catch (AuthTokenException e) {
-                    Log.e("AUTHORIZATION FAILED", e.getMessage());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<SignupResponse> call, Throwable t) {
-                CustomToast.showToastShort(SignupActivity.this, "Failed to sign up");
-                Log.e("ERROR", "Couldn't  sign up", t);
-            }
-        });
-    }
-=======
 package com.example.pricetracker;
 
 import android.content.Intent;
@@ -145,5 +70,4 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
     }
->>>>>>> 65b55aacd8480322c23cf22321aea25884d4a02f
 }
--- app/src/main/java/com/example/pricetracker/api/provider/ItemServiceProvider.java
+++ app/src/main/java/com/example/pricetracker/api/provider/ItemServiceProvider.java
@@ -1,69 +1,3 @@
-<<<<<<< HEAD
-package com.example.pricetracker.api.provider;
-
-import com.example.pricetracker.api.ItemService;
-import com.example.pricetracker.api.ServerUrls;
-import com.example.pricetracker.api.headers.AuthTokenException;
-import com.example.pricetracker.api.headers.AuthorizationUtils;
-import com.example.pricetracker.dto.request.FollowItemRequest;
-import com.example.pricetracker.dto.response.FollowedItemResponse;
-import com.example.pricetracker.dto.response.ItemPriceResponse;
-import com.example.pricetracker.dto.response.ItemResponse;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-public class ItemServiceProvider {
-
-    private static ItemServiceProvider instance = null;
-    private final ItemService itemService;
-    private final String authToken;
-
-    private ItemServiceProvider() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(ServerUrls.SERVER_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        this.itemService = retrofit.create(ItemService.class);
-        try {
-            this.authToken = AuthorizationUtils.getAuthTokenFormatted();
-        } catch (AuthTokenException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static ItemServiceProvider getInstance() {
-        if(instance == null) {
-            instance = new ItemServiceProvider();
-        }
-        return instance;
-    }
-
-    public Call<List<ItemResponse>> getItems() {
-        return itemService.getItems(authToken);
-    }
-
-
-    public Call<List<ItemResponse>> getFollowedItems() {
-        return itemService.getFollowedItems(authToken);
-    }
-
-    public Call<List<ItemPriceResponse>> getItemPrices(int itemId) {
-        return itemService.getItemPrices(authToken, itemId);
-    }
-
-    public Call<FollowedItemResponse> followItem(FollowItemRequest followItemRequest) {
-        return itemService.followItem(authToken, followItemRequest);
-    }
-
-    public Call<FollowedItemResponse> unfollowItem(FollowItemRequest followItemRequest) {
-        return itemService.unfollowItem(authToken, followItemRequest);
-    }
-}
-=======
 package com.example.pricetracker.api.provider;
 
 import com.example.pricetracker.api.ItemService;
@@ -96,7 +30,7 @@ public class ItemServiceProvider {
     }
 
     public static ItemServiceProvider getInstance() {
-        if(instance == null) {
+        if (instance == null) {
             instance = new ItemServiceProvider();
         }
         return instance;
@@ -122,4 +56,3 @@ public class ItemServiceProvider {
         return itemService.unfollowItem(authorizationProvider.getAuthTokenFormatted(), followItemRequest);
     }
 }
->>>>>>> 65b55aacd8480322c23cf22321aea25884d4a02f
